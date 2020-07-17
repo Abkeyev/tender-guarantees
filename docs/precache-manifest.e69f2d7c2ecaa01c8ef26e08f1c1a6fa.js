cbe8249c0c35588a0b2b65ee313edd90e3d2e831
Abkeyev Daniyar <abkeyev@gmail.com>
2020-07-17 09:42:34 +0600

    init

---
+++ docs/precache-manifest.e69f2d7c2ecaa01c8ef26e08f1c1a6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c541139ebe77437d783f3e8c0207c747",
+    "url": "/tender-guarantees/index.html"
+  },
+  {
+    "revision": "5e6ff13aeb528d37c88b",
+    "url": "/tender-guarantees/static/css/main.aa1da0c2.chunk.css"
+  },
+  {
+    "revision": "b79bdec18410d23e1e76",
+    "url": "/tender-guarantees/static/js/2.2af58904.chunk.js"
+  },
+  {
+    "revision": "ba3b4500f00880cf9656c39900077397",
+    "url": "/tender-guarantees/static/js/2.2af58904.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e6ff13aeb528d37c88b",
+    "url": "/tender-guarantees/static/js/main.0574bfed.chunk.js"
+  },
+  {
+    "revision": "5089caf9c0466d6066c4",
+    "url": "/tender-guarantees/static/js/runtime-main.1d26d8dc.js"
+  }
+]);
