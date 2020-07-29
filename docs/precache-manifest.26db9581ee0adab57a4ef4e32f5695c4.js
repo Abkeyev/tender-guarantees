428f3463b55bda051d7fbdbfd988426957122418
Abkeyev Daniyar <abkeyev@gmail.com>
2020-07-29 08:10:08 +0600

    publish on git

--- docs/precache-manifest.26db9581ee0adab57a4ef4e32f5695c4.js
+++ docs/precache-manifest.26db9581ee0adab57a4ef4e32f5695c4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2eebd8a28768db099e339813fb1754f6",
+    "revision": "1b569a5f0fcf1876b8d61d699220c791",
     "url": "/tender-guarantees/index.html"
   },
   {
-    "revision": "59d45ef4e71a9ecb8ef3",
+    "revision": "7ed3c98bd9305b59ecb0",
     "url": "/tender-guarantees/static/css/main.aa1da0c2.chunk.css"
   },
   {
-    "revision": "b79bdec18410d23e1e76",
-    "url": "/tender-guarantees/static/js/2.2af58904.chunk.js"
+    "revision": "f565b1a996932c51ccb2",
+    "url": "/tender-guarantees/static/js/2.bf9f220f.chunk.js"
   },
   {
     "revision": "ba3b4500f00880cf9656c39900077397",
-    "url": "/tender-guarantees/static/js/2.2af58904.chunk.js.LICENSE"
+    "url": "/tender-guarantees/static/js/2.bf9f220f.chunk.js.LICENSE"
   },
   {
-    "revision": "59d45ef4e71a9ecb8ef3",
-    "url": "/tender-guarantees/static/js/main.5a2ff8bd.chunk.js"
+    "revision": "7ed3c98bd9305b59ecb0",
+    "url": "/tender-guarantees/static/js/main.691aa4e5.chunk.js"
   },
   {
     "revision": "5089caf9c0466d6066c4",
